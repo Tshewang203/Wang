88f14e94cfe4e62f38d50d33b9440eb1a3fe9aa4
Tshewang203 <tshewanglhamo466@gmail.com>
2025-04-09 15:14:22 +0600

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import RegistrationForm from './components/RegistrationForm';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Student Semester Registration</h1>
+      <RegistrationForm />
+    </div>
+  );
+}
